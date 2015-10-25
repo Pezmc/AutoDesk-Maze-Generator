8fdcfb3df737a61bced85d727d442c9ef846761b
Pez Cuckow <email@pezcuckow.com>
2015-10-25 02:30:40 +0000

    Use the maze from CurrentMaze.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ var urn = b.toString('base64');
 
 var currentMaze = fs.readFileSync("CurrentMaze.js", { encoding: 'utf8' }).replace(/(\n|\r)+$/, '');
 
-fs.writeFileSync('./www/urn.js', 'urn = "' + urn +'"; maze = ' + currentMaze + ';');
+fs.writeFileSync('./www/urn.js', 'urn = "urn:' + urn +'"; maze = ' + currentMaze + ';');
 
 var server = app.listen(app.get('port'), function() {
     console.log('Server listening on port ' + server.address().port);
